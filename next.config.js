6707c4b8ce1a36efea6a49ce81178f6e3ca19673
abcworld123 <rkaxhdals@gmail.com>
2022-05-11 23:47:37 +0900

    Init project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': `${__dirname}/components`,
+      '@styles': `${__dirname}/styles`,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
